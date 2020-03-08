cdbced286f821d88534da39fa1fbd848dd445f0f
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-08 16:06:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,7 +102,14 @@ end
  end
  
   def self.all_students_in_grade_X 
-    
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
     
   
