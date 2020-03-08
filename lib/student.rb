e124c1399ec848988275c85ba234964de42bed47
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-08 15:52:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,10 +24,11 @@ class Student
   end
 
   def self.new_from_db(row)
+    
     id = row[0]
     name = row[1]
     grade = row[2]
-    new_student = self.new(id, name, grade)
+    
     new_student
   end
 
